574b0b5efaafb03822f33b61e2833931fa6c3da0
Walter Czencz <wallyczencz@yahoo.com.ar>
2023-10-10 13:08:35 -0300

    Cambios de estilo y formulario contacto

---
+++ script2.js
@@ -0,0 +1,14 @@
+
+let footer =  `    <h3>Encontranos en nuestras redes</h3>
+<div class="footer">
+    <a href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+    <a href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+    <a href="http//:www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
+</div>
+<div class="copyright"><i class="fa-solid fa-copyright"></i>
+    <p>Desarrollado por:</p>
+    <div class="div"></div>
+    
+</div>
+<img class="imagen" src="imagenes/footer.jpg" alt="fondo de mascotas">`
+document.querySelector("footer").innerHTML = footer;

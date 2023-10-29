d8bbf75fc7c13b95ebb06f56cf8a5bfffcb3fda2
Walter Czencz <wallyczencz@yahoo.com.ar>
2023-10-29 19:55:35 -0300

    ig

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ let footer =  `    <h3 id="txtfooter">Encontranos en nuestras redes</h3>
 <div class="footer">
     <a href="http://www.twitter.com" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
     <a href="http://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-    <a href="http//www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
+    <a href="http://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
 </div>
 <div class="copyright"><i class="fa-solid fa-copyright"></i>
     <p>Desarrollado por:Grupo 17, Comision 23517,<br> 
